d329242c0b92224a189259f4d5a8b3f507e7a251
Dhruvi Patel <pate1255@algonquinlive.com>
2024-12-08 18:12:31 -0500

    fetch api from the backend and installed vue and vite

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+    <Suspense>
+        <template #default>
+            <RouterView />
+        </template>
+        <template #fallback>
+            <!-- Loading -->
+        </template>
+    </Suspense>
+
+</template>
+
+
